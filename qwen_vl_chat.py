05feff762389057eee31225bf335fe543c894afe
isuyun <sylee@islab.snu.ac.kr>
2025-04-28 13:49:43 +0900

    task: stack the cups

--- qwen_vl_chat.py
+++ qwen_vl_chat.py
@@ -124,7 +124,7 @@ if __name__ == "__main__":
         "/home/sylee/codes/Qwen2.5-VL/images/stack_cups3.jpg"
     ]
     image_ids = [1, 2, 3]  # or actual file names if you prefer
-    task = "Stack the cups by placing the red first, blue inside, purple on top." #"Clean up the desk."
+    task = "Stack the cups." #"Clean up the desk."
 
     output_json_path = "/home/sylee/codes/Qwen2.5-VL/outputs/stack_cups.json"
 
